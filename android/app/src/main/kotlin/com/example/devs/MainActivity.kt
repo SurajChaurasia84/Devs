80692f9d79cbe5debf347ba83c8e5423a8ad4635
Suraj Chaurasia <suraj84chaurasia@gmail.com>
2026-06-11 00:18:05 +0530

    refactor(android): update package structure and MainActivity

--- android/app/src/main/kotlin/com/example/devs/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.devs
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/com/suuraj/devs/app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.suuraj.devs.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
