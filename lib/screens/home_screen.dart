99c968238089b68e2c81afe0f172c44922d95d2f
Suraj Chaurasia <suraj84chaurasia@gmail.com>
2026-06-24 01:53:16 +0530

    feat(home): improve home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/material.dart';
+﻿import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:lucide_icons/lucide_icons.dart';
+import 'package:lucide_icons_flutter/lucide_icons.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:google_fonts/google_fonts.dart';
 import '../data/mock_data.dart';
