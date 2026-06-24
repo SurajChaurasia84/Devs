7b6c3c620792cbb5732864fe515a8147db100c02
Suraj Chaurasia <suraj84chaurasia@gmail.com>
2026-06-24 20:48:00 +0530

    test: update widget tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,7 +4,7 @@ import 'package:devs/main.dart';
 void main() {
   testWidgets('Devs app smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MyApp(isDarkMode: true));
 
     // Verify that "Devs" logo or text is rendered in the UI.
     expect(find.text('Devs'), findsWidgets);
